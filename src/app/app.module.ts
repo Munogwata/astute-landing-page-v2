b986aa3a6feafa999c8011e5b8856af3c467281d
Munozovepi Gwata <muno.gwata@gmail.com>
2022-06-28 07:45:44 +0200

    AMT-4 added the components that need to built for thsi ticket

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponentsComponent } from './navigation-components/navigation-components.component';
+import { UpdateBarComponent } from './navigation-components/update-bar/update-bar.component';
+import { FundingStatusComponent } from './navigation-components/funding-status/funding-status.component';
+import { MenuBarComponent } from './navigation-components/menu-bar/menu-bar.component';
+import { FooterComponent } from './navigation-components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponentsComponent,
+    UpdateBarComponent,
+    FundingStatusComponent,
+    MenuBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
