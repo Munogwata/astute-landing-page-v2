8219cc0f9789b7f8388369606321c43d5cd8af06
Munozovepi Gwata <muno.gwata@gmail.com>
2022-06-28 08:06:28 +0200

    AMT-4 added the main pages and the routing for purposes of navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavigationComponentsComponent } from './navigation-components/navigation-components.component';
-import { UpdateBarComponent } from './navigation-components/update-bar/update-bar.component';
+import { FooterComponent } from './navigation-components/footer/footer.component';
 import { FundingStatusComponent } from './navigation-components/funding-status/funding-status.component';
 import { MenuBarComponent } from './navigation-components/menu-bar/menu-bar.component';
-import { FooterComponent } from './navigation-components/footer/footer.component';
+import { UpdateBarComponent } from './navigation-components/update-bar/update-bar.component';
+import { HomeMoneyComponent } from './pages/home-money/home-money.component';
+import { MakeComponent } from './pages/make/make.component';
+import { KeepComponent } from './pages/keep/keep.component';
+import { GrowComponent } from './pages/grow/grow.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponentsComponent,
     UpdateBarComponent,
-    FundingStatusComponent,
     MenuBarComponent,
-    FooterComponent
+    FundingStatusComponent,
+    FooterComponent,
+    HomeMoneyComponent,
+    MakeComponent,
+    KeepComponent,
+    GrowComponent,
+    AboutUsComponent
+
   ],
   imports: [
     BrowserModule,
