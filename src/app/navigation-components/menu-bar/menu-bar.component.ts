c3ca753bd60f36ff33ad924b1ffb2ad977b81f68
Munozovepi Gwata <muno.gwata@gmail.com>
2022-06-29 20:01:20 +0200

    AMT-4 the menu-bar is completed with routing and is responsive

--- src/app/navigation-components/menu-bar/menu-bar.component.ts
+++ src/app/navigation-components/menu-bar/menu-bar.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuBarComponent implements OnInit {
 
+  open = false;
+  close = true;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleMenu() {
+    this.open = !this.open
+    this.close = !this.close
   }
 
 }
